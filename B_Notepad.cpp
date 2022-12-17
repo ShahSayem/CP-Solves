19873ec6c9b91d87044eb76c4082912d033dd654
Shah Sayem Ahmad <shahsayemahmad@gmail.com>
2022-12-17 16:27:22 +0600

    17 Dec

--- B_Notepad.cpp
+++ B_Notepad.cpp
@@ -6,7 +6,7 @@ using namespace std;
 typedef long long ll;
 const long double pi = 3.14159265358979323846;
 const ll MOD = 1e9+7;
-const int MAX = 1000000;
+const int MAX = 10000000;
 
 ///.........Graph.........///
 //vector <int> adj[10000000];
@@ -17,18 +17,25 @@ void solve()
 {
     int n;
     cin>>n;
+    string s;
+    cin>>s;
 
-    int ans = 0, x = 1, lim = 10;
-    for (int i = 1; i <= n; i+= x){
-            ans++;
+    map <string, int> mp;
+    for (int i = 0; i < n-1; i++){
+        string ss = "";
+        ss += s[i];
+        ss += s[i+1];
 
-        if (i == lim){
-            lim *= 10;
-            x *= 10;
+        if (!mp[ss])
+            mp[ss] = i+1;
+
+        else if (mp[ss] < i){
+            cout<<"YES";
+            return;
         }
     }
     
-    cout<<ans;
+    cout<<"NO";
 }
 
 int main()
