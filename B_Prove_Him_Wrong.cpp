605b0bbb91aeb3d414732054df27a41c4a7ddadd
Shah Sayem Ahmad <shahsayemahmad@gmail.com>
2023-08-11 11:23:19 +0600

    11 Aug

---
+++ B_Prove_Him_Wrong.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Shah_Sayem ios_base::sync_with_stdio(false);cin.tie(NULL);
+typedef long long ll;
+const int MAX = 10000000+5;
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+
+    vector <int> v;
+    for (int i = 1; i <= n/2; i++){
+        v.push_back(i);
+    }
+}
+
+int main()
+{
+    // Shah_Sayem
+
+    int tc = 1;
+    cin>>tc;
+    while (tc--){
+        solve();
+        cout<<"\n";
+    }
+
+    return 0;
+}
